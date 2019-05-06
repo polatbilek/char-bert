bfb95ff61c822fafb6bb40c1d38357e212d178d9
polatbilek <polatbilekozan@hotmail.com>
2019-05-06 13:58:15 +0300

    preprocess and embedding reading done, only skeleton is created on model

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from flags import FLAGS
 from model import Model
 
 
-data = preprocess.get_data(FLAGS.dataset_path)
+training_data, test_data = preprocess.get_data(FLAGS.dataset_path)
 embeddings = preprocess.read_embeddings(FLAGS.embedding_path)
 
-model = Model()
+model = Model(training_data, test_data, embeddings, "Embedding average")
 model.train()
 model.test()
--- model.py
+++ model.py
@@ -2,12 +2,19 @@ from flags import FLAGS
 
 #TODO: Implement the model's training and test, make it as smooth as state change
 class Model:
-	def __init__(self):
-		pass
+	def __init__(self, training_data, test_data, embeddings, mode):
+		self.mode = mode
+		self.training_data = training_data
+		self.test_data = test_data
+		self.embeddings = embeddings
 
 	def train(self):
-		pass
+		if self.mode == "Embedding average":
+			self.embedding_average()
 
 	def test(self):
+		pass
+
+	def embedding_average(self):
 		pass
 
