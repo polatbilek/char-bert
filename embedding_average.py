e556d29748c06a93e3894372e0617d65615806e1
polatbilek <polatbilekozan@hotmail.com>
2019-05-08 16:26:46 +0300

    Instead of Model class, now each approach will have its own class, but implement train() and test().

--- embedding_average.py
+++ embedding_average.py
@@ -1,20 +1,15 @@
 from flags import FLAGS
 
 #TODO: Implement the model's training and test, make it as smooth as state change
-class Model:
-	def __init__(self, training_data, test_data, embeddings, mode):
-		self.mode = mode
+class Embed_Average:
+	def __init__(self, training_data, test_data, embeddings):
 		self.training_data = training_data
 		self.test_data = test_data
 		self.embeddings = embeddings
 
 	def train(self):
-		if self.mode == "Embedding average":
-			self.embedding_average()
-
-	def test(self):
 		pass
 
-	def embedding_average(self):
+	def test(self):
 		pass
 
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import preprocess
 from flags import FLAGS
-from model import Model
-
+from embedding_average import Embed_Average
 
 training_data, test_data = preprocess.get_data(FLAGS.dataset_path)
 embeddings = preprocess.read_embeddings(FLAGS.embedding_path)
 
-model = Model(training_data, test_data, embeddings, "Embedding average")
+model = Embed_Average(training_data, test_data, embeddings)
+
 model.train()
 model.test()
